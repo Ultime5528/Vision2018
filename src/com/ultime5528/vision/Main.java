9f15d0f5f5d24bda903d2de8befd1017cd39a960
Unknown <etienne.beaulac@me.com>
2018-03-27 17:36:06 -0400

    Affichage du temps et FPS

--- src/com/ultime5528/vision/Main.java
+++ src/com/ultime5528/vision/Main.java
@@ -24,9 +24,9 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		
-		
-		
+
+
+
 		ImageViewer viewer = new ImageViewer();
 		viewer.setSize(600, 400);
 		viewer.setVisible(true);
@@ -42,91 +42,96 @@ public class Main {
 		viewer.putNumber("Blur", 1, 50, 25);
 		viewer.putNumber("Temps", 0, 135, 135);
 
-		long start  = System.currentTimeMillis();
-		
-		while(cap.isOpened()) {
+		long start;
 
+		while(cap.isOpened()) {
+			
+			start = System.currentTimeMillis();
+			
 			if(cap.read(imgOrigine) && imgOrigine != null) {
-				
+
 				int blurValue = viewer.getNumber("Blur");
-				
+
 				Imgproc.blur(imgOrigine, imgModif, new Size(blurValue, blurValue));
 				Imgproc.putText(imgModif, blurValue + "", new Point(0, imgModif.rows()), Core.FONT_HERSHEY_DUPLEX, 3, new Scalar(255, 255, 255), 3);
 
 				imgOrigine.copyTo(imageTexte);
 				ecrireInfo(imageTexte, viewer.getNumber("Temps"), false, true);
-				
-				
-				
+
+
+
 				long time = System.currentTimeMillis() - start;
-				
+				ecrireFps(imageTexte, time);
+
+			
 				
 			}
 
 			//viewer.putImage("Image originale", VisionUtils.createAwtImage(imgOrigine));
-			//viewer.putImage("Image modifiée", VisionUtils.createAwtImage(imgModif));
+			//viewer.putImage("Image modifiï¿½e", VisionUtils.createAwtImage(imgModif));
 			viewer.putImage("Image Texte", VisionUtils.createAwtImage(imageTexte));
+
+
+
 			
-			
-			
-			 start = System.currentTimeMillis();
 
 		}
 
 	}
-	
+
 	public static void ecrireFps( Mat img, long time) {
 		int hauteur = img.rows();
 		int largeur = img.cols();
-		
+
 		Scalar couleur;
-		
+
 		couleur = new Scalar(255, 255, 255);
+
+		Imgproc.putText(img, Math.round(1000.0 / time) + " fps", new Point(largeur * 0.85 , (hauteur * 0.5)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(255, 255, 255), 2);
 		
-		Imgproc.putText(img, (1.0 / time) + " fps", new Point(largeur * 0.5 , (hauteur * 0.5)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(255, 255, 255), 2);
-		
+		Imgproc.putText(img, time + " ms" , new Point(largeur * 0.85 , (hauteur * 0.05)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(255, 255, 255), 2);
 	}
-	
-	
-	
-	
+
+
+
+
 	public static void ecrireInfo( Mat img, int tempsRestant, boolean cube, boolean intake ) {
-		
+
 		int hauteur = img.rows();
 		int largeur = img.cols();
-		
+
 		Scalar couleur;
-		
+
 		if(tempsRestant >= 30){
 			couleur = new Scalar(0, 255, 0);
-			
+
 		}
-		
+
 		else{
-			
+
 			couleur = new Scalar(0, 255, 255);
-			
+
 		}
-		
+
 		Imgproc.rectangle(img, new Point(0, hauteur * 0.9) , new Point(largeur* tempsRestant / 135.0, hauteur), couleur, -1, 8, 0);
-		
+
 		Size tailleTexte = Imgproc.getTextSize(tempsRestant + "", Core.FONT_HERSHEY_DUPLEX, 1, 3, null);
 		Imgproc.putText(img, tempsRestant + "", new Point((largeur / 2 ) - (tailleTexte.width / 2) , (hauteur * 0.97)), Core.FONT_HERSHEY_DUPLEX, 1, new Scalar(0, 0, 255), 3);
-		
+
 		Imgproc.putText(img, "Intake:", new Point(0 , (hauteur * 0.05)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(255, 255, 255), 2);
-		
+
 		if(intake){
 			Imgproc.putText(img, "ON", new Point(largeur * 0.14 , (hauteur * 0.05)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(0, 255, 0), 2);
 		}
 		else{
 			Imgproc.putText(img, "OFF", new Point(largeur * 0.14 , (hauteur * 0.05)), Core.FONT_HERSHEY_COMPLEX_SMALL, 1, new Scalar(0, 0, 255), 2);
 		}
-		
+
 		Imgproc.rectangle(img, new Point(0, hauteur * 0.05),new Point(0, largeur * 0.5) , new Scalar(255, 255, 255), -1, 8, 0);
-		
+
 	}
-	
-	
+
+
 }
 
 
